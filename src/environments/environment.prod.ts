6e4c5c5e2457407b73717536af57aaa5c3a3bc41
Octavian Popa <tavi.popa@gitscom.ro>
2021-03-01 00:19:00 +0200

    Prod env new backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     firebaseAPIKey: 'AIzaSyA5F4jvBwS1Fuv1qR29ImSKKv9CMGhaQkc',
-    apiUrl: 'http://app.ebook.ro:8080'
+    apiUrl: 'http://app.ebook.ro:8081/bm'
 };
